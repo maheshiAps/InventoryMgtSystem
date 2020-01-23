9c40dbd5b36aa684effd8c1ae841a90d9f647040
Maheshi Apsara <48525627+maheshiAps@users.noreply.github.com>
2020-01-24 00:00:11 +0530

    Add files via upload

---
+++ scripts.php
@@ -0,0 +1,10 @@
+	<script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+  
+    <script src="js/jquery-confirm.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+	
+	
+	
